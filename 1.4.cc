ee2537ea498c61049a0edfeb32bd2f2741db35f8
yegle <yegle@ifanr.com>
2014-01-05 00:19:25 -0500

    Fix: a small typo

--- 1.4.cc
+++ 1.4.cc
@@ -8,7 +8,7 @@ void insert_at(char* base, char* insertion, int pos) {
 void right_shift(char* str, int len, int pos, int distance, char padding) {
 }
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
     if (argc != 3) {
         cout << "Usage: " << argv[0] << " string_to_be_replaced true_length" << endl;
         return 1;

39d4b5f414747b9f0f6c7d0560e58944dd4a194d
yegle <yegle@ifanr.com>
2014-01-12 14:26:58 -0500

    Mod: working 1.4.cc

--- 1.4.cc
+++ 1.4.cc
@@ -2,16 +2,40 @@
 
 using namespace std;
 
-void insert_at(char* base, char* insertion, int pos) {
+void replace(char* str, int true_length) {
+    auto space_count = 0;
+
+    for(auto i=0; i<true_length; i++) {
+        if (str[i] == ' ') {
+            space_count++;
+        }
+    }
+
+    auto index = true_length-1;
+    auto space_left = space_count;
+    while(index != 0) {
+        if(str[index] == ' ') {
+            space_left--;
+            str[index+2*space_left] = '%';
+            str[index+2*space_left+1] = '2';
+            str[index+2*space_left+2] = '0';
+        } else {
+            str[index+2*space_left] = str[index];
+        }
+        index--;
+    }
 }
 
-void right_shift(char* str, int len, int pos, int distance, char padding) {
-}
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         cout << "Usage: " << argv[0] << " string_to_be_replaced true_length" << endl;
         return 1;
     }
+    char buf[1024];
+    strncpy(buf, argv[1], 1024);
+    cout << "Before: " << buf << endl;
+    replace(buf, atoi(argv[2]));
+    cout << "After: " << buf << endl;
     return 0;
 }
